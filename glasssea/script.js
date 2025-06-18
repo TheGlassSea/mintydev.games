ac717efc020e8946c74aec892975b08e4df4be88
Eve <62806964+TheCoolerMS3@users.noreply.github.com>
2025-06-19 00:09:58 +0100

    Add files via upload

---
+++ glasssea/script.js
@@ -0,0 +1,4 @@
+if (window.location.hostname == "theglasssea.eve.hackclub.app"){
+document.getElementById("urlnotice").innerHTML = '<div style="background-color: blue; padding: 10px; color: aliceblue;">We have aquired a domain! Check out <a href="https://mintydev.games/glasssea">mintydev.games</a>!!</div>';
+console.log("hi");
+}
